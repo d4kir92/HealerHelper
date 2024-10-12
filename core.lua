9763175bc184080339e8dad53efb4b1f56a53f05
D4KiR <23246564+d4kir92@users.noreply.github.com>
2024-10-12 20:40:59 +0200

    v0.3.0

--- core.lua
+++ core.lua
@@ -1,7 +1,6 @@
-local _, HealerHelper = ...
+local AddonName, HealerHelper = ...
 local MAXROW = 5
 local MAX = 10
-local LAYOUT = "RIGHT" -- "BOTTOM", "RIGHT"
 local ActionButtonCastType = {
     Cast = 1,
     Channel = 2,
@@ -9,13 +8,20 @@ local ActionButtonCastType = {
 }
 
 local healerHelper = CreateFrame("Frame")
-healerHelper:RegisterEvent("PLAYER_LOGIN")
+healerHelper:RegisterEvent("ADDON_LOADED")
 healerHelper:SetScript(
     "OnEvent",
-    function(sel, ...)
+    function(sel, event, addonName)
+        if addonName ~= AddonName then return end
         HEAHELPC = HEAHELPC or {}
+        HEAHELPC["LAYOUT"] = HEAHELPC["LAYOUT"] or "BOTTOM"
+        HEAHELPC["GAP"] = HEAHELPC["GAP"] or 6
+        HEAHELPC["OFFSET"] = HEAHELPC["OFFSET"] or 2
+        HEAHELPC["RLAYOUT"] = HEAHELPC["RLAYOUT"] or "BOTTOM"
+        HEAHELPC["RGAP"] = HEAHELPC["RGAP"] or 6
+        HEAHELPC["ROFFSET"] = HEAHELPC["ROFFSET"] or 2
         HealerHelper:SetAddonOutput("HealerHelper", "134149")
-        HealerHelper:SetVersion("HealerHelper", "134149", 0.1)
+        HealerHelper:InitSettings()
         local healBars = {}
         hooksecurefunc(
             "CompactUnitFrame_SetUpFrame",
@@ -27,7 +33,98 @@ healerHelper:SetScript(
             end
         )
 
-        HealerHelper:MSG(string.format("LOADED v%s", 0.1))
+        local function FindDirection()
+            if CompactPartyFrame then
+                local sw, sh = CompactPartyFrame:GetSize()
+                if sw > sh then
+                    return "RIGHT"
+                else
+                    return "DOWN"
+                end
+            elseif CompactRaidFrame then
+                local sw, sh = CompactRaidFrame:GetSize()
+                if sw > sh then
+                    return "RIGHT"
+                else
+                    return "DOWN"
+                end
+            else
+                HealerHelper:MSG("[FindDirection] FAILED")
+            end
+
+            return "FAILED"
+        end
+
+        local function UpdateFramePosition(frame, i)
+            if frame:GetName() and string.match(frame:GetName(), "CompactPartyFrameMember") then
+                local bar = _G["HealerHelper_BAR_" .. frame:GetName()]
+                if bar then
+                    if HEAHELPC["LAYOUT"] == "BOTTOM" then
+                        if bar then
+                            bar:ClearAllPoints()
+                            bar:SetPoint("TOP", frame, "BOTTOM", 0, -HEAHELPC["OFFSET"])
+                        end
+                    elseif HEAHELPC["LAYOUT"] == "RIGHT" then
+                        if bar then
+                            bar:ClearAllPoints()
+                            bar:SetPoint("LEFT", frame, "RIGHT", HEAHELPC["OFFSET"], 0)
+                        end
+                    else
+                        HealerHelper:MSG("MISSING LAYOUT", HEAHELPC["LAYOUT"])
+                    end
+
+                    local direction = FindDirection()
+                    local spacingY = 0
+                    local spacingX = 0
+                    if i > 1 then
+                        local previousFrame = _G["CompactPartyFrameMember" .. (i - 1)]
+                        if previousFrame then
+                            frame:ClearAllPoints()
+                            if direction == "DOWN" then
+                                if HEAHELPC["LAYOUT"] == "RIGHT" and direction == "DOWN" then
+                                    spacingY = HEAHELPC["GAP"]
+                                elseif HEAHELPC["LAYOUT"] == "BOTTOM" and direction == "DOWN" then
+                                    spacingY = bar:GetHeight() * bar:GetScale() + HEAHELPC["GAP"] + HEAHELPC["OFFSET"]
+                                end
+
+                                frame:SetPoint("TOPLEFT", previousFrame, "BOTTOMLEFT", 0, -spacingY)
+                            else
+                                if HEAHELPC["LAYOUT"] == "RIGHT" and direction == "RIGHT" then
+                                    spacingX = bar:GetWidth() * bar:GetScale() + HEAHELPC["GAP"] + HEAHELPC["OFFSET"]
+                                elseif HEAHELPC["LAYOUT"] == "BOTTOM" and direction == "RIGHT" then
+                                    spacingX = HEAHELPC["GAP"]
+                                end
+
+                                frame:SetPoint("LEFT", previousFrame, "RIGHT", spacingX, 0)
+                            end
+                        end
+                    end
+                end
+            end
+        end
+
+        local test = false
+        function HealerHelper:UpdateHealBarsLayout()
+            if test then return end
+            test = true
+            for i = 1, MEMBERS_PER_RAID_GROUP do
+                local frame = _G["CompactPartyFrameMember" .. i]
+                if frame then
+                    UpdateFramePosition(frame, i)
+                end
+            end
+
+            test = false
+        end
+
+        hooksecurefunc(
+            "CompactUnitFrame_UpdateAll",
+            function()
+                HealerHelper:UpdateHealBarsLayout()
+            end
+        )
+
+        HealerHelper:MSG(string.format("LOADED v%s", "0.3.0"))
     end
 )
 
@@ -48,33 +145,21 @@ function HealerHelper:ClearSpell(btn)
     btn.icon:SetTexture(nil)
 end
 
-function HealerHelper:HandleBtn(unitFrame, btn, i)
-    local btnsLength = btn:GetWidth() * MAXROW
+function HealerHelper:HandleBtn(bar, btn, i)
     if not InCombatLockdown() then
-        btn:SetScale(unitFrame:GetWidth() / btnsLength)
         btn:ClearAllPoints()
-        if LAYOUT == "RIGHT" then
-            if i > MAXROW then
-                btn:SetPoint("TOPLEFT", unitFrame, "TOPRIGHT", (i - 1 - MAXROW) * btn:GetWidth(), -btn:GetHeight())
-            else
-                btn:SetPoint("TOPLEFT", unitFrame, "TOPRIGHT", (i - 1) * btn:GetWidth(), 0)
-            end
-        elseif LAYOUT == "BOTTOM" then
-            if i > MAXROW then
-                btn:SetPoint("TOPLEFT", unitFrame, "BOTTOMLEFT", (i - 1 - MAXROW) * btn:GetWidth(), -btn:GetHeight())
-            else
-                btn:SetPoint("TOPLEFT", unitFrame, "BOTTOMLEFT", (i - 1) * btn:GetWidth(), 0)
-            end
+        if i > MAXROW then
+            btn:SetPoint("TOPLEFT", bar, "TOPLEFT", (i - 1 - MAXROW) * btn:GetWidth(), -btn:GetHeight())
         else
-            HealerHelper:MSG("Missing Layout", LAYOUT)
+            btn:SetPoint("TOPLEFT", bar, "TOPLEFT", (i - 1) * btn:GetWidth(), 0)
         end
     end
 end
 
 function HealerHelper:AddActionButton(frame, bar, i)
-    local name = frame.GetName and frame:GetName() or nil
+    local name = bar.GetName and bar:GetName() or nil
     if name == nil then return end
-    local customButton = CreateFrame("CheckButton", name .. "_HealerHelper_" .. i, frame, "SecureActionButtonTemplate, ActionButtonTemplate")
+    local customButton = CreateFrame("CheckButton", name .. "_BTN_" .. i, bar, "SecureActionButtonTemplate, ActionButtonTemplate")
     customButton:RegisterUnitEvent("UNIT_SPELLCAST_START", "player")
     customButton:RegisterUnitEvent("UNIT_SPELLCAST_EMPOWER_START", "player")
     customButton:RegisterUnitEvent("UNIT_SPELLCAST_SUCCEEDED", "player")
@@ -157,16 +242,22 @@ function HealerHelper:AddActionButton(frame, bar, i)
     hooksecurefunc(
         frame,
         "SetWidth",
-        function()
-            HealerHelper:HandleBtn(frame, customButton, i)
+        function(sel, sw)
+            bar:SetSize(sw, sw / MAXROW * 2)
+            local scale = sw / (customButton:GetWidth() * MAXROW)
+            customButton:SetScale(scale)
+            HealerHelper:HandleBtn(bar, customButton, i)
         end
     )
 
     hooksecurefunc(
         frame,
         "SetSize",
-        function()
-            HealerHelper:HandleBtn(frame, customButton, i)
+        function(sel, sw, sh)
+            bar:SetSize(sw, sw / MAXROW * 2)
+            local scale = sw / (customButton:GetWidth() * MAXROW)
+            customButton:SetScale(scale)
+            HealerHelper:HandleBtn(bar, customButton, i)
         end
     )
 
@@ -205,7 +296,6 @@ function HealerHelper:AddActionButton(frame, bar, i)
                 if spellName then
                     HEAHELPC["spell" .. i] = nil
                     HealerHelper:ClearSpell(sel)
-                    print(spellName)
                     C_Spell.PickupSpell(spellName)
                 end
             end
@@ -214,7 +304,15 @@ function HealerHelper:AddActionButton(frame, bar, i)
 end
 
 function HealerHelper:AddHealbar(frame)
-    for i = 1, MAX do
-        HealerHelper:AddActionButton(frame, bar, i)
+    if frame and frame:GetName() ~= nil then
+        local bar = CreateFrame("Frame", "HealerHelper_BAR_" .. frame:GetName(), frame)
+        bar:SetSize(10, 10)
+        bar:SetPoint("CENTER", frame, "CENTER", 0, 0)
+        --[[ bar.t = bar:CreateTexture()
+        bar.t:SetColorTexture(1, 0, 0)
+        bar.t:SetAllPoints(bar)]]
+        for i = 1, MAX do
+            HealerHelper:AddActionButton(frame, bar, i)
+        end
     end
 end
--- settings.lua
+++ settings.lua
@@ -1,1 +1,144 @@
---local AddonName, HealerHelper = ...
+local _, HealerHelper = ...
+HealerHelper:AddTrans("enUS", "GENERAL", "General")
+HealerHelper:AddTrans("enUS", "MMBTN", "Show Minimapbutton")
+HealerHelper:AddTrans("enUS", "PARTY", "Party")
+HealerHelper:AddTrans("enUS", "LAYOUT", "Party Layout")
+HealerHelper:AddTrans("enUS", "GAP", "Party Gap: %s")
+HealerHelper:AddTrans("enUS", "OFFSET", "Party Offset: %s")
+HealerHelper:AddTrans("enUS", "RAID", "Raid")
+HealerHelper:AddTrans("enUS", "RLAYOUT", "Raid Layout")
+HealerHelper:AddTrans("enUS", "RGAP", "Raid Gap: %s")
+HealerHelper:AddTrans("enUS", "ROFFSET", "Raid Offset: %s")
+local heahel_settings = nil
+function HealerHelper:ToggleSettings()
+    if heahel_settings:IsShown() then
+        heahel_settings:Hide()
+    else
+        heahel_settings:Show()
+    end
+end
+
+function HealerHelper:InitSettings()
+    HealerHelper:SetVersion("HealerHelper", "134149", "0.3.0")
+    heahel_settings = HealerHelper:CreateFrame(
+        {
+            ["name"] = "HealerHelper",
+            ["pTab"] = {"CENTER"},
+            ["sw"] = 520,
+            ["sh"] = 520,
+            ["title"] = format("HealerHelper |T134149:16:16:0:0|t v|cff3FC7EB%s", "0.3.0")
+        }
+    )
+
+    heahel_settings.SF = CreateFrame("ScrollFrame", "heahel_settings_SF", heahel_settings, "UIPanelScrollFrameTemplate")
+    heahel_settings.SF:SetPoint("TOPLEFT", heahel_settings, 8, -26)
+    heahel_settings.SF:SetPoint("BOTTOMRIGHT", heahel_settings, -32, 8)
+    heahel_settings.SC = CreateFrame("Frame", "heahel_settings_SC", heahel_settings.SF)
+    heahel_settings.SC:SetSize(heahel_settings.SF:GetSize())
+    heahel_settings.SC:SetPoint("TOPLEFT", heahel_settings.SF, "TOPLEFT", 0, 0)
+    heahel_settings.SF:SetScrollChild(heahel_settings.SC)
+    local y = 0
+    HealerHelper:SetAppendY(y)
+    HealerHelper:SetAppendParent(heahel_settings.SC)
+    HealerHelper:SetAppendTab(HEAHELPC)
+    HealerHelper:AppendCategory("GENERAL")
+    HealerHelper:AppendCheckbox(
+        "MMBTN",
+        HealerHelper:GetWoWBuild() ~= "RETAIL",
+        function(sel, val)
+            if val then
+                HealerHelper:ShowMMBtn("HealerHelper")
+            else
+                HealerHelper:HideMMBtn("HealerHelper")
+            end
+        end
+    )
+
+    HealerHelper:AppendCategory("PARTY")
+    HealerHelper:AppendDropdown(
+        "LAYOUT",
+        "BOTTOM",
+        {
+            ["BOTTOM"] = "BOTTOM",
+            ["RIGHT"] = "RIGHT"
+        }
+    )
+
+    HealerHelper:AppendSlider(
+        "OFFSET",
+        2,
+        0,
+        20,
+        1,
+        0,
+        function()
+            HealerHelper:UpdateHealBarsLayout()
+        end
+    )
+
+    HealerHelper:AppendSlider(
+        "GAP",
+        6,
+        0,
+        20,
+        1,
+        0,
+        function()
+            HealerHelper:UpdateHealBarsLayout()
+        end
+    )
+
+    HealerHelper:AppendCategory("RAID")
+    HealerHelper:AppendDropdown(
+        "RLAYOUT",
+        "BOTTOM",
+        {
+            ["BOTTOM"] = "BOTTOM",
+            ["RIGHT"] = "RIGHT"
+        }
+    )
+
+    HealerHelper:AppendSlider(
+        "ROFFSET",
+        2,
+        0,
+        20,
+        1,
+        0,
+        function()
+            HealerHelper:UpdateHealBarsLayout()
+        end
+    )
+
+    HealerHelper:AppendSlider(
+        "RGAP",
+        6,
+        0,
+        20,
+        1,
+        0,
+        function()
+            HealerHelper:UpdateHealBarsLayout()
+        end
+    )
+
+    HealerHelper:AddSlash("healerhelper", HealerHelper.ToggleSettings)
+    HealerHelper:AddSlash("heahel", HealerHelper.ToggleSettings)
+    HealerHelper:CreateMinimapButton(
+        {
+            ["name"] = "HealerHelper",
+            ["icon"] = 134149,
+            ["dbtab"] = HEAHELPC,
+            ["vTT"] = {{"HealerHelper |T134149:16:16:0:0|t", "v|cff3FC7EB0.4.42"}, {"Leftclick", "Toggle Settings"}},
+            ["funcL"] = function()
+                HealerHelper:ToggleSettings()
+            end,
+        }
+    )
+
+    if HealerHelper:GV(HEAHELPC, "MMBTN", HealerHelper:GetWoWBuild() ~= "RETAIL") then
+        HealerHelper:ShowMMBtn("HealerHelper")
+    else
+        HealerHelper:HideMMBtn("HealerHelper")
+    end
+end
