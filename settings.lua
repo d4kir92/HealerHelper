7b2dd248314e20ff6d33af1b52342d76ec1d931a
D4KiR <23246564+d4kir92@users.noreply.github.com>
2025-04-19 11:28:48 +0200

    v0.7.39

--- settings.lua
+++ settings.lua
@@ -10,7 +10,7 @@ function HealerHelper:ToggleSettings()
 end
 
 function HealerHelper:InitSettings()
-    HealerHelper:SetVersion(134149, "0.7.38")
+    HealerHelper:SetVersion(134149, "0.7.39")
     heahel_settings = HealerHelper:CreateFrame(
         {
             ["name"] = "HealerHelper",
