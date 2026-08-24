1f58801dc2403f92751071bc9fa551c78b8f16de
D4KiR <23246564+d4kir92@users.noreply.github.com>
2026-08-24 15:11:13 +0200

    v0.7.68

--- settings.lua
+++ settings.lua
@@ -10,7 +10,7 @@ function HealerHelper:ToggleSettings()
 end
 
 function HealerHelper:InitSettings()
-    HealerHelper:SetVersion(134149, "0.7.67")
+    HealerHelper:SetVersion(134149, "0.7.68")
     heahel_settings = HealerHelper:CreateWindow({
         ["name"] = "HealerHelper",
         ["pTab"] = {"CENTER"},
